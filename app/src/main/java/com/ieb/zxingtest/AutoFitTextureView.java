11365560a3b5086a720d419ab0199d382b96e757
Iain Ballard <iain.ballard@gmail.com>
2025-07-24 14:26:40 +0100

    basic working scanner

---
+++ app/src/main/java/com/ieb/zxingtest/AutoFitTextureView.java
@@ -0,0 +1,87 @@
+package com.ieb.zxingtest;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+
+public class AutoFitTextureView extends TextureView {
+    private final String TAG = "CamView";
+    private int mRatioWidth = 0;
+    private int mRatioHeight = 0;
+
+    public AutoFitTextureView(Context context) {
+        this(context, null);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /**
+     * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
+     * calculated from the parameters. Note that the actual sizes of parameters don't matter, that
+     * is, calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
+     *
+     * @param width  Relative horizontal size
+     * @param height Relative vertical size
+     */
+    public void setAspectRatio(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+        Log.i(TAG, "Aspect ratio updated");
+        mRatioWidth = width;
+        mRatioHeight = height;
+        requestLayout();
+    }
+
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = View.MeasureSpec.getSize(widthMeasureSpec);
+        int height = View.MeasureSpec.getSize(heightMeasureSpec);
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
+    }
+
+    public void handleClick() {
+        Bitmap bitmap = null;
+        try {
+            bitmap = getBitmap();
+            if (bitmap == null){
+                Log.w(TAG, "Failed to read texture bmp");
+                return;
+            }
+
+            Log.w(TAG, "Captured: w="+bitmap.getWidth()+"; h="+bitmap.getHeight()+";");
+
+            // Grab a random pixel for testing
+            var pix = bitmap.getPixel(10, 10);
+            var guess = (pix | (pix >> 8) | (pix >> 16)) & 0xFF;
+
+            if (guess > 127) Log.w(TAG, "Image is bright?");
+            else Log.w(TAG, "Image is dark?");
+        } catch (Throwable t) {
+            Log.e(TAG, "Failed to get bitmap: " + t);
+        } finally {
+            if (bitmap != null) bitmap.recycle();
+        }
+    }
+}
---
+++ app/src/main/java/com/ieb/zxingtest/Main.java
@@ -0,0 +1,137 @@
+package com.ieb.zxingtest;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.widget.LinearLayout.VERTICAL;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.common.GlobalHistogramBinarizer;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+
+/** @noinspection NullableProblems*/
+public class Main extends Activity
+{
+    private final String TAG = "MainAct";
+    private CameraController camControl;
+    private AutoFitTextureView previewTexture;
+    private TextView text;
+
+    private volatile boolean updateInProgress;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        // Setup layout
+        updateInProgress = false;
+        var root = new LinearLayout(this);
+        root.setOrientation(VERTICAL);
+        setContentView(root);
+
+        text = new TextView(this);
+        previewTexture = new AutoFitTextureView(this);
+        camControl = new CameraController(this, previewTexture, surfaceTexture -> updateReading());
+
+        root.addView(text, new LinearLayout.LayoutParams(MATCH_PARENT, 0,1));
+        root.addView(previewTexture, new LinearLayout.LayoutParams(MATCH_PARENT, 0,2));
+
+        text.append("\r\nRunning test. Point camera at a QR code...");
+
+        // Start camera feeding into the preview control
+        camControl.onResume();
+    }
+
+    private void tryToFindQrCodeInBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            Log.w(TAG, "Invalid bitmap");
+            return;
+        }
+
+        try {
+            // Transfer to ZXing format
+            int w = bitmap.getWidth();
+            int h = bitmap.getHeight();
+            var pixels = new int[w*h];
+            bitmap.getPixels(pixels, 0, w, 0, 0, w, h);
+            var image = new RGBLuminanceSource(w,h, pixels);
+
+            // Threshold down to a black&white image
+            //var thresholder = new GlobalHistogramBinarizer(image); // faster, but not as robust
+            var thresholder = new HybridBinarizer(image);
+            var binMap = new BinaryBitmap(thresholder);
+
+            // Try to read the QR code image
+            var reader = new QRCodeReader();
+            var result = reader.decode(binMap);
+
+            text.setText("\r\nFound QR code");
+            text.append("\r\nBar code result: " + result.toString());
+            text.append("\r\nBar code type: " + result.getBarcodeFormat().toString());
+
+        } catch (com.google.zxing.NotFoundException nf) {
+            // No QR code found. This is fine
+        } catch (Exception e) {
+            // If the capture is blurry or not at a good angle, we probably get a checksum error here.
+            Log.w(TAG, "Could not read bar-code: "+e);
+        }
+    }
+
+    /** Try to read a QR code from the current texture */
+    private void updateReading() {
+        if (updateInProgress) {
+            return; // QR scanner is not keeping up with camera preview rate. Not really a problem.
+        }
+        updateInProgress = true;
+
+
+        Bitmap bitmap = null;
+        try {
+            bitmap = previewTexture.getBitmap();
+            if (bitmap == null){
+                Log.w(TAG, "Failed to read texture bmp");
+                return;
+            }
+
+            tryToFindQrCodeInBitmap(bitmap);
+        } catch (Throwable t) {
+            Log.e(TAG, "Failed to get bitmap: " + t);
+        } finally {
+            if (bitmap != null) bitmap.recycle();
+            updateInProgress = false;
+        }
+    }
+
+
+    /** This is triggered if we had to request permission from the user */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Re-trigger camera control
+        camControl.onResume();
+    }
+
+    /** pause camera control */
+    @Override
+    protected void onPause() {
+        camControl.onPause();
+        super.onPause();
+    }
+
+    /** resume camera control */
+    @Override
+    protected void onResume() {
+        camControl.onResume();
+        super.onResume();
+    }
+}
